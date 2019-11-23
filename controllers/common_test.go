b4e8bed580b34e24cbe686ecd32ce1c7047ef219
Cade Daniel <edacih@gmail.com>
2019-11-22 18:08:28 -0800

    Making GetGeneratedJobName robust to different maxNameLens and adding unit tests.

---
+++ controllers/common_test.go
@@ -0,0 +1,147 @@
+/*
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/google/uuid"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("GetGeneratedJobName", func() {
+
+	var (
+		uid            types.UID
+		objectMetaName string
+		maxNameLen     int
+
+		uidWithoutHyphens string
+
+		generatedJobName string
+	)
+
+	BeforeEach(func() {
+		uid = types.UID(uuid.New().String())
+	})
+
+	JustBeforeEach(func() {
+		uidWithoutHyphens = strings.Replace(string(uid), "-", "", -1)
+		generatedJobName = GetGeneratedJobName(uid, objectMetaName, maxNameLen)
+	})
+
+	When("maxNameLen is sufficiently large", func() {
+
+		BeforeEach(func() {
+			maxNameLen = 64
+			objectMetaName = "object.meta.name"
+		})
+
+		It("Concatenates the name and shortened uid", func() {
+			Expect(generatedJobName).To(ContainSubstring(objectMetaName))
+			Expect(generatedJobName).To(ContainSubstring(uidWithoutHyphens))
+		})
+
+		It("Length does not exceed maxNameLen", func() {
+			Expect(len(generatedJobName)).To(BeNumerically("<=", maxNameLen))
+		})
+	})
+
+	When("maxNameLen is exactly enough", func() {
+
+		BeforeEach(func() {
+			maxNameLen = 64
+			uidWithoutHyphensLength := 32
+			delimiterLength := 1
+			objectMetaName = strings.Repeat("A", (maxNameLen - (uidWithoutHyphensLength + delimiterLength)))
+		})
+
+		It("Concatenates the name and shortened uid", func() {
+			Expect(generatedJobName).To(ContainSubstring(objectMetaName))
+			Expect(generatedJobName).To(ContainSubstring(uidWithoutHyphens))
+		})
+
+		It("Length does not exceed maxNameLen", func() {
+			Expect(len(generatedJobName)).To(BeNumerically("<=", maxNameLen))
+		})
+	})
+
+	When("maxNameLen is not large enough for entire objectMetaName", func() {
+
+		Context("Due to maxNameLen being smaller", func() {
+			BeforeEach(func() {
+				maxNameLen = 32
+				objectMetaName = "object.meta.name"
+			})
+
+			It("Contains the full UID", func() {
+				Expect(generatedJobName).To(ContainSubstring(uidWithoutHyphens))
+			})
+
+			It("Length does not exceed maxNameLen", func() {
+				Expect(len(generatedJobName)).To(BeNumerically("<=", maxNameLen))
+			})
+
+			It("Does not start with a hypthen", func() {
+				Expect(generatedJobName[0]).ToNot(Equal("-"))
+			})
+		})
+
+		Context("Due to objectMetaName being larger", func() {
+			BeforeEach(func() {
+				maxNameLen = 64
+
+				// Kubernetes max is 253.
+				// https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
+				objectMetaName = strings.Repeat("A", 253)
+			})
+
+			It("Contains the full UID", func() {
+				Expect(generatedJobName).To(ContainSubstring(uidWithoutHyphens))
+			})
+
+			It("Length does not exceed maxNameLen", func() {
+				Expect(len(generatedJobName)).To(BeNumerically("<=", maxNameLen))
+			})
+
+			It("Does not start with a hypthen", func() {
+				Expect(generatedJobName[0]).ToNot(Equal("-"))
+			})
+
+		})
+	})
+
+	When("maxNameLen is not large enough for objectMetaName or full UID", func() {
+
+		BeforeEach(func() {
+			// UID without hyphens is 32.
+			maxNameLen = 30
+			objectMetaName = "object.meta.name"
+		})
+
+		It("Equals the truncated UID", func() {
+			Expect(generatedJobName).To(Equal(uidWithoutHyphens[:maxNameLen]))
+		})
+
+		It("Length does not exceed maxNameLen", func() {
+			Expect(len(generatedJobName)).To(BeNumerically("<=", maxNameLen))
+		})
+	})
+})
---
+++ controllers/suite_test.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+func TestControllerCommon(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Controller Common Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+})
