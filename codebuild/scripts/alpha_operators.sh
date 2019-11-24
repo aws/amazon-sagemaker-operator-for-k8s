f183b273e32f95898e0ad6827f0f6b692b005c5d
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2019-11-24 21:05:19 +0000

    Fixed alpha repo name

--- codebuild/scripts/alpha_operators.sh
+++ codebuild/scripts/alpha_operators.sh
@@ -4,7 +4,7 @@ source codebuild/scripts/package_operators.sh
 $(aws ecr get-login --no-include-email --region $ALPHA_REPOSITORY_REGION --registry-ids $ALPHA_ACCOUNT_ID)
 
 # Build the image with a temporary tag
-ALPHA_IMAGE=$ALPHA_ACCOUNT_ID.dkr.ecr.$ALPHA_REPOSITORY_REGION.amazonaws.com/$image_repository
+ALPHA_IMAGE=$ALPHA_ACCOUNT_ID.dkr.ecr.$ALPHA_REPOSITORY_REGION.amazonaws.com/$REPOSITORY_NAME
 make docker-build docker-push IMG=$ALPHA_IMAGE:$CODEBUILD_RESOLVED_SOURCE_VERSION
 
 # Release the operator into the private alpha repository
