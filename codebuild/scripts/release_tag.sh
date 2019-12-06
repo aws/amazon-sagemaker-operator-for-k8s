8ad7797eb865fde177068378cb564f55fe23af80
Nicholas Thomson <nithomso@amazon.com>
2019-12-06 20:31:35 +0000

    Float release semver up to major and minor tags

--- codebuild/scripts/release_tag.sh
+++ codebuild/scripts/release_tag.sh
@@ -18,10 +18,10 @@ function retag_image()
   docker push $image:$new_tag
 }
 
-CODEBUILD_GIT_TAG="$(git describe --tags --exact-match 2>/dev/null)"
+GIT_TAG="$(git describe --tags --exact-match 2>/dev/null)"
 
 # Only run the release process for tagged commits
-if [ "$CODEBUILD_GIT_TAG" == "" ]; then
+if [ "$GIT_TAG" == "" ]; then
   exit 0
 fi
 
@@ -45,9 +45,17 @@ for row in $(echo ${ACCOUNTS_ESCAPED} | jq -r '.[] | @base64'); do
 
   image=${repository_account}.dkr.ecr.${region}.amazonaws.com/${image_repository}
   old_tag="${CODEBUILD_RESOLVED_SOURCE_VERSION}"
-  new_tag="${CODEBUILD_GIT_TAG}"
+  full_tag="${GIT_TAG}"
 
-  echo "Tagging $image:$old_tag to $image:$new_tag"
+  # Get minor and major version tags
+  [[ $GIT_TAG =~ ^v[0-9]+\.[0-9]+ ]] && minor_tag="${BASH_REMATCH[0]}"
+  [[ $GIT_TAG =~ ^v[0-9]+ ]] && major_tag="${BASH_REMATCH[0]}"
 
-  retag_image "$image" "$old_tag" "$new_tag"
+  echo "Tagging $region with $full_tag"
+
+  retag_image "$image" "$old_tag" "$full_tag"
+  retag_image "$image" "$old_tag" "$minor_tag"
+  retag_image "$image" "$old_tag" "$major_tag"
+
+  echo "Finished tagging $region with $full_tag"
 done
