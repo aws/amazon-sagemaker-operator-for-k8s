33fe5d3165555612910156c324c95a60afcfaa0f
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2020-01-21 11:29:28 -0800

    Added integration tests for "delete" verb (#74)
    * Created delete function
    
    * Created verify_delete method
    
    * Added Training, HPO and Batch delete tests
    
    * Fixed incorrect parameter
    
    * Create model before running batch
    
    * Fixed test directories
    
    * Use yq to modify model name
    
    * Added YQ apt repository
    
    * Reformatted integration test yaml
    
    * Reformatted sample test yaml
    
    * Removed duplicate configuration settings
    
    * Refactored integration and canary scripts
    
    * Increased timeout and refactored timeout code
    
    * Added SM delete checks
    
    * Renamed wait_for_crd_status
    
    * Refactored tests into their own files
    
    * Moved all run_test functionality into logical files
    
    * Formatting changes
    
    * Cast CRD type to lowercase
    
    * Added TODO in create_tests import

--- tests/codebuild/run_all_sample_canary_tests.sh
+++ tests/codebuild/run_all_sample_canary_tests.sh
@@ -1,44 +1,12 @@
 #!/bin/bash
 
-source run_test.sh
+source create_tests.sh
+source delete_tests.sh
+source inject_tests.sh
+source smlogs_tests.sh
 
-
-# Inject environment variables into tests
-inject_variables tests/xgboost-mnist-trainingjob.yaml
-inject_variables tests/xgboost-mnist-hpo.yaml
-inject_variables tests/xgboost-model.yaml
-inject_variables tests/xgboost-mnist-batchtransform.yaml 
-inject_variables tests/xgboost-hosting-deployment.yaml
-
-# Add all your new sample files below
-# Run test
-# Format: `run_test tests/<Your test file name>`
-run_test tests/xgboost-mnist-trainingjob.yaml
-run_test tests/xgboost-mnist-hpo.yaml
-run_test tests/xgboost-model.yaml
-# Special code for batch transform till we fix issue-59
-run_test tests/xgboost-model.yaml
-# We need to get sagemaker model before running batch transform
-verify_test Model xgboost-model 1m Created
-sed -i "s/xgboost-model/$(get_sagemaker_model_from_k8s_model xgboost-model)/g" tests/xgboost-mnist-batchtransform.yaml
-run_test tests/xgboost-mnist-batchtransform.yaml 
-run_test tests/xgboost-hosting-deployment.yaml
-
-# Verify test
-# Format: `verify_test <type of job> <Job's metadata name> <timeout to complete the test> <desired status for job to achieve>` 
-verify_test TrainingJob xgboost-mnist 20m Completed
-verify_test HyperparameterTuningJob xgboost-mnist-hpo 20m Completed
-verify_test BatchTransformJob xgboost-batch 30m Completed
-verify_test HostingDeployment hosting 40m InService
-
-# Verify smlogs worked.
-# TODO this is common with run_all_sample_test. Should put in own file.
-if [ "$(kubectl smlogs trainingjob xgboost-mnist | wc -l)" -lt "1" ]; then
-    echo "smlogs trainingjob did not produce any output."
-    exit 1
-fi
-
-if [ "$(kubectl smlogs batchtransformjob xgboost-batch | wc -l)" -lt "1" ]; then
-    echo "smlogs batchtransformjob did not produce any output."
-    exit 1
-fi
+run_canary_tests
+verify_canary_tests
+run_smlogs_canary_tests
+delete_all_resources # Delete all existing resources to re-use metadata names
+run_delete_canary_tests
--- tests/codebuild/run_all_sample_test.sh
+++ tests/codebuild/run_all_sample_test.sh
@@ -1,67 +1,12 @@
 #!/bin/bash
 
-source run_test.sh
+source create_tests.sh
+source delete_tests.sh
+source inject_tests.sh
+source smlogs_tests.sh
 
-# Build prerequisite resources
-if [ "$FSX_ID" == "" ]; then
-  echo "Skipping build_fsx_from_s3 as fsx tests are disabled"
-  #build_fsx_from_s3
-fi
-
-# TODO: Automate creation/testing of EFS file systems for relevant jobs
-
-# Inject environment variables into tests
-inject_variables testfiles/xgboost-mnist-trainingjob.yaml
-inject_variables testfiles/spot-xgboost-mnist-trainingjob.yaml
-inject_variables testfiles/xgboost-mnist-custom-endpoint.yaml
-# inject_variables testfiles/efs-xgboost-mnist-trainingjob.yaml
-#inject_variables testfiles/fsx-kmeans-mnist-trainingjob.yaml
-inject_variables testfiles/xgboost-mnist-hpo.yaml
-inject_variables testfiles/spot-xgboost-mnist-hpo.yaml
-inject_variables testfiles/xgboost-mnist-hpo-custom-endpoint.yaml
-inject_variables testfiles/xgboost-model.yaml
-inject_variables testfiles/xgboost-mnist-batchtransform.yaml
-inject_variables testfiles/xgboost-hosting-deployment.yaml
-
-# Add all your new sample files below
-# Run test
-# Format: `run_test testfiles/<Your test file name>`
-run_test testfiles/xgboost-mnist-trainingjob.yaml
-run_test testfiles/spot-xgboost-mnist-trainingjob.yaml
-run_test testfiles/xgboost-mnist-custom-endpoint.yaml
-# run_test testfiles/efs-xgboost-mnist-trainingjob.yaml
-#run_test testfiles/fsx-kmeans-mnist-trainingjob.yaml
-run_test testfiles/xgboost-mnist-hpo.yaml
-run_test testfiles/spot-xgboost-mnist-hpo.yaml
-run_test testfiles/xgboost-mnist-hpo-custom-endpoint.yaml
-run_test testfiles/xgboost-model.yaml
-# Special function for batch transform till we fix issue-59
-run_test testfiles/xgboost-model.yaml
-# We need to get sagemaker model before running batch transform
-verify_test Model xgboost-model 1m Created
-sed -i "s/xgboost-model/$(get_sagemaker_model_from_k8s_model xgboost-model)/g" testfiles/xgboost-mnist-batchtransform.yaml
-run_test testfiles/xgboost-mnist-batchtransform.yaml 
-run_test testfiles/xgboost-hosting-deployment.yaml
-
-# Verify test
-# Format: `verify_test <type of job> <Job's metadata name> <timeout to complete the test> <desired status for job to achieve>` 
-verify_test trainingjob xgboost-mnist 20m Completed
-verify_test trainingjob spot-xgboost-mnist 20m Completed
-verify_test trainingjob xgboost-mnist-custom-endpoint 20m Completed
-# verify_test trainingjob efs-xgboost-mnist 20m Completed
-#verify_test trainingjob fsx-kmeans-mnist 20m Completed
-verify_test HyperparameterTuningJob xgboost-mnist-hpo 20m Completed
-verify_test HyperparameterTuningJob spot-xgboost-mnist-hpo 20m Completed
-verify_test HyperparameterTuningJob xgboost-mnist-hpo-custom-endpoint 20m Completed
-verify_test BatchTransformJob xgboost-batch 30m Completed
-verify_test HostingDeployment hosting 40m InService
-
-# Verify smlogs worked.
-if [ "$(kubectl smlogs trainingjob xgboost-mnist | wc -l)" -lt "1" ]; then
-    echo "smlogs trainingjob did not produce any output."
-    exit 1
-fi
-if [ "$(kubectl smlogs batchtransformjob xgboost-batch | wc -l)" -lt "1" ]; then
-    echo "smlogs batchtransformjob did not produce any output."
-    exit 1
-fi
+run_integration_tests
+verify_integration_tests
+run_smlogs_integration_tests
+delete_all_resources # Delete all existing resources to re-use metadata names
+run_delete_integration_tests
