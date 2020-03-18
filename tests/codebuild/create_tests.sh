256db8e9c290f64b2f106a0684e275cff584660d
Gautam Kumar <gautamnitc@gmail.com>
2020-03-18 12:20:57 -0700

    Add Integration test for debugger job (#103)
    * Add debug status for debuger job
    
    * Changing the command from describe to get

--- tests/codebuild/create_tests.sh
+++ tests/codebuild/create_tests.sh
@@ -67,6 +67,28 @@ function verify_integration_tests
   verify_test HyperparameterTuningJob spot-xgboost-mnist-hpo 20m Completed
   verify_test HyperparameterTuningJob xgboost-mnist-hpo-custom-endpoint 20m Completed
   verify_test TrainingJob xgboost-mnist-debugger 20m Completed
+  # Verify that debug job has status
+  verify_debug_test TrainingJob xgboost-mnist-debugger NoIssuesFound
+}
+
+# This function verifies that a given debug job has specific status
+# Parameter:
+#    $1: CRD type
+#    $2: Instance of CRD
+#    $3: Single debug job status
+function verify_debug_test
+{
+  local crd_type="$1"
+  local crd_instance="$2"
+  local expected_debug_job_status="$3"
+  # TODO extend this for multiple debug job with debug job statuses parameter
+  local found_debug_job_status="$(kubectl get "$crd_type" "$crd_instance" -o json | jq -r '.status.debugRuleEvaluationStatuses[0].ruleEvaluationStatus')"
+  if [ "${expected_debug_job_status}" == "${found_debug_job_status}" ]; then
+      echo "[PASSED] Verified ${crd_type} ${crd_instance} debug job has status ${expected_debug_job_status}"
+  else
+      echo "[FAILED] ${crd_type} ${crd_instance} debug job has status ${found_debug_job_status} while expected status was ${expected_debug_job_status}"
+      exit 1
+  fi
 }
 
 # This function verifies that job has started and not failed
