e779cb09829fd939577f49ef518a7225f0a258e8
Kartik Kalamadi <kalamadi@amazon.com>
2021-02-09 15:24:37 -0800

    enable smlog

--- codebuild/scripts/package_alpha_operators.sh
+++ codebuild/scripts/package_alpha_operators.sh
@@ -12,7 +12,7 @@ ALPHA_IMAGE=$ALPHA_ACCOUNT_ID.dkr.ecr.$ALPHA_REPOSITORY_REGION.amazonaws.com/$RE
 make docker-build docker-push IMG=$ALPHA_IMAGE:$CODEBUILD_RESOLVED_SOURCE_VERSION
 
 # Ensure the smlogs are built and tarred into the alpha bucket
-SHOULD_PUSH_SMLOGS_ALPHA="false"
+SHOULD_PUSH_SMLOGS_ALPHA="true"
 
 # Release the operator into the private alpha repository
 # Set as all to ensure it runs through the function
