6f678dff33248a573595a2db85367a18e0f09458
Meghna Baijal <mbaijal@amazon.com>
2021-12-06 11:02:03 -0800

    Update EKS Cluster Version to 1.21

--- tests/codebuild/run_canarytest.sh
+++ tests/codebuild/run_canarytest.sh
@@ -37,7 +37,7 @@ source ./common.sh
 # Build environment `Docker image` has all prerequisite setup and credentials are being passed using AWS system manager
 
 CLUSTER_REGION=${CLUSTER_REGION:-us-east-1}
-CLUSTER_VERSION=1.17
+CLUSTER_VERSION=1.21
 
 # Define the list of optional subnets for the EKS test cluster
 CLUSTER_PUBLIC_SUBNETS=${CLUSTER_PUBLIC_SUBNETS:-}
